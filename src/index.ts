5100152acba7d0a6013350f998d4d00a445878eb
junek3 <rlarudrms28@gmail.com>
2018-07-19 01:56:39 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-const sayhi = (name:string, age:number, gender:string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!!`;
+class Human {
+    public name:string;
+    private age:number;
+    public gender:string;
+    constructor(name:string, age:number, gender:string){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+    }
+}
+
+const lynn = new Human("Lynn", 18, "female");
+
+const sayhi = (person:Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`;
 };
 
-console.log(sayhi("Damien", 20, "male"));
+console.log(sayhi(lynn));
 export {};
