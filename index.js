5ee0d385d087adc40b540eb4bfc9b2ad6c39b215
rlarudrms28 <35721370+JuneK3@users.noreply.github.com>
2018-07-18 21:33:31 +0900

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Damien", age = 20, gender = "male";
-const sayhi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayhi(name, age);
-//# sourceMappingURL=index.js.map
