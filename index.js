933b6be5ef21cc283a27d0e47a52177afc1818a1
junek3 <rlarudrms28@gmail.com>
2018-07-18 20:31:32 +0900

    TypeChain

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert("Hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Damien", age = 20, gender = "male";
+const sayhi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayhi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-alert("Hello");
+const name="Damien",
+    age=20,
+    gender="male";
+
+const sayhi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayhi(name, age, gender);
+export {};
